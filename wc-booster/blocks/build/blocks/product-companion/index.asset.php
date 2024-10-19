408ad449a1167a03eb4e23f8d88c43645bbe23e2
Sean Murphy <sean@iamseanmurphy.com>
2024-10-19 01:06:11 +0000

    2.4

---
+++ wc-booster/blocks/build/blocks/product-companion/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'b893fa1c70133de2c0e4');
